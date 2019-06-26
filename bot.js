691af3c73cdc8368bdd69d65ad91387ee2494c6b
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2019-06-27 02:26:59 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,194 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "%";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on("message", message => {
+ if (message.content === "%help%") {
+        message.react("😮")
+                message.react("🤒")
+  const embed = new Discord.RichEmbed()
+      .setColor("#ffff00")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(`
+     
+     
+● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●
+👑『 اوامر البوت』👑
+ 
+👑%ban 『لتعطي شخص باند』
+ 
+👑%kick 『لتعطي شخص كيك』
+ 
+👑%clear 『لمسح الشات برقم』
+ 
+👑%skin  『يوريك اسكنك في ماين كرافت 』
+ 
+👑%embed 『خاصيه غرد لكن بغير طريقه』
+ 
+👑%bc  『خيارات البرودكاست』
+ 
+👑%user 『لمعرفة معلومات حسابك في السيرفر』
+
+`)
+ 
+ 
+message.author.sendEmbed(embed)
+ 
+}
+});
+
+ client.on('message', (message) => {
+    if (message.content.startsWith('%kick')) {
+        var member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(member.displayName + ' تم طرد هذا الشخص من السيرفر');
+        }).catch(() => {
+            message.channel.send(":x:");
+        });
+    }
+});
+
+client.on('message' , message => {
+  if (message.author.dark) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+      /*let banlog = client.channels.find("name", "ban-log");
+  if(!banlog) return message.reply("I've detected that this server doesn't have a ban-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الباند**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني ابند شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).ban(7, user);
+  message.channel.sendMessage("**لقد تم اعطاء الباند الي شخص بنجاح** ✅");
+}
+});
+
+
+
+client.on("message", message => {
+  var prefix = "%";
+  if(message.content.startsWith(prefix + "embed")) {
+    
+
+var color = message.content.split(" ")[1];
+  var text = message.content.split(" ").slice(2);
+    var tt = text.join(" ")
+  if(!color) return message.reply("يجب كتابة لون الامبد الذي تريده");
+    if(!tt) return message.reply("يجب كتابة كلام لتكراره");
+  let embed = new Discord.RichEmbed()
+  .setColor(color)
+  .setDescription(tt)
+  message.channel.send(embed).catch(Julian =>{console.log('`Error`: ' + Julian);
+message.channel.send("`Error`:" + Julian)
+    })
+  }
+  });
+
+client.on('message', message => {
+  const aa = message.content.split(" ").slice(1).join(" ");
+  if(message.content.startsWith(prefix + "skin")){
+    if(!aa) return message.reply(`:x:  -  **${prefix}skin <name>**`);
+    var ss = new Discord.RichEmbed()
+    .setTitle(`${aa}'s Skin!`)
+    .setURL(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setThumbnail(`https://minotar.net/avatar/${aa}`)
+    .setImage(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setFooter(`Requested By : ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(ss);
+  }
+});
+
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + 'send')) {
+    if(!message.author.id === "569502505289908245") return;
+    var user = message.mentions.members.first();
+    var args = message.content.split(" ").slice(1).join(" ");
+user.send(args);
+  }});
+
+
+client.on('message', message => {
+  if(message.content === prefix + "user"){
+    var embed = new Discord.RichEmbed()
+    .setTitle(message.author.tag, message.author.avatarURL)
+    .addField(`User`, message.author.username)
+    .addField(`Discrim`,`#`+ message.author.discriminator)
+    .addField(`Name Color Role`, message.member.colorRole)
+    .addField(`Game`,message.author.presence.game ||"Idel.")
+    .addField(`Status`,message.author.presence.status)
+    message.channel.send(embed);
+  }
+});
+
+client.on('guildMemberAdd', Sal => { //By Salto7#4595
+    var embed = new Discord.RichEmbed()
+    .setAuthor(Sal.user.username, Sal.user.avatarURL)
+    .setThumbnail(Sal.user.avatarURL)
+    .setImage('https://cdn.discordapp.com/attachments/492862340484694027/493771573740830740/welcome1.png') //هنا حط الصوره الي تبيها
+    .setTitle('عضو جديد!')
+    .setDescription('مرحبا بك بالسيرفر')
+    .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
+    .addField('``تاق العضو``', Sal.user.discriminator, true)
+    .addField('``تم الانشاء في``', Sal.user.createdAt, true)
+    .addField(' 👤  انت رقم',`**[ ${Sal.guild.memberCount} ]**`,true)
+    .setColor('RANDOM')
+    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+    var channel =Sal.guild.channels.find('name', ' (￦𝐄𝐋𝐶𝐎𝑴𝙀)') // هنا حط اسم الروم الي تبيه يكتب فيه
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {  ///الامر
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
+ message.delete(); 
+};     
+});
+
+client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+    return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+  :crown:اسم العضو  ${member}:crown:  
+  انت العضو رقم ${member.guild.memberCount} `) 
+  }).catch(console.error)
+  })
+
+client.on('guildMemberAdd', member => {
+  
+  const channel = member.guild.channels.find(ch => ch.name === '￦𝐄𝐋𝐶𝐎𝑴𝙀');
+ 
+  if (!channel) return;
+
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","「 🆉🆇🆉」"));
+    });
+
+client.login('NTgyODg0ODU4OTkzMzExNzQ1.XRNMLQ.CbmzP0SQUict3e2ud2b_y5SJ0n0');
